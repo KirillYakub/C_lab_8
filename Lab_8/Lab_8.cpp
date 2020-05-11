4c5dfa18bc6e2f9211f8e73547e569df16c03955
KirillYakub <kirillyakubchinskiy@gmail.com>
2020-05-11 19:55:58 +0300

    Оптимизировано!

--- Lab_8/Lab_8.cpp
+++ Lab_8/Lab_8.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <forward_list>
 #include <list>
 #include <iterator>
 
@@ -6,16 +7,16 @@ using namespace std;
 
 void easy()
 {
-    /* Создать список из целых чисел. Вставить числа 111 и 222 перед первым нулевым элементом. */
+    /* Создать однонаправленный список из целых чисел. Вставить числа 111 и 222 перед первым нулевым элементом. */
 
     //создаем список и итератор списка;
-    list <int> Numbers = { 5, 8, 0, -2, 9, 0, 6 };
+    forward_list <int> Numbers = { 5, 8, 0, -2, 9, 0, 6 };
 
-    list <int> ::iterator Place;
+    forward_list <int> ::iterator Place;
 
     cout << "Элементы списка перед добавлением: ";
 
-    //выводи на экран изначальный список;
+    //выводим на экран изначальный список;
     for (Place = Numbers.begin(); Place != Numbers.end(); Place++)
     {
         cout << *Place << " ";
@@ -23,11 +24,11 @@ void easy()
 
     cout << endl << endl;
 
-    //переводим итератор от начала на два символа вперед, добавляем элементы в список;
+    //переводим итератор от начала на один символ вперед - перед первым нулевым элементом, добавляем элементы в список;
     Place = Numbers.begin();
-    std::advance(Place, 2);
-    Numbers.insert(Place, 111);
-    Numbers.insert(Place, 222);
+    advance(Place, 1);
+    Numbers.insert_after(Place, 222);
+    Numbers.insert_after(Place, 111);
 
     cout << "Элементы после добавления: ";
 
@@ -41,19 +42,19 @@ void easy()
 }
 
 //если элемент больше 25 - функция вернет его для удаления из списка;
-bool number_single(const double& val)
+bool number_more_than_twentyfive(const double& number)
 {
-    return val > 25;
+    return number > 25;
 }
 
 void medium()
 {
-    /*Дан список вещественных чисел. Найти среднее значение чисел меньших либо равных 15.
+    /*Дан однонаправленный список вещественных чисел. Найти среднее значение чисел меньших либо равных 15.
       Удалить из списка элементы, которые больше чем 25. */
 
-    list <double> Numbers = { 5, 8.2, 15.4, 9.1, 20.7, 26, 31.2, -4 };
+    forward_list <double> Numbers = { 5, 8.2, 15.4, 9.1, 20.7, 26, 31.2, -4 };
 
-    list <double> ::iterator Place;
+    forward_list <double> ::iterator Place;
 
     double sum = 0;
     int count = 0;
@@ -78,7 +79,7 @@ void medium()
     //удаляем элементы большие чем 25;
     for (Place = Numbers.begin(); Place != Numbers.end(); Place++)
     {
-        Numbers.remove_if(number_single);
+        Numbers.remove_if(number_more_than_twentyfive);
     }
 
     cout << "Элементы списка после удаления элементов больших чем 25: ";
@@ -93,45 +94,56 @@ void medium()
 
 void hard()
 {
-    /* Создать циклический список их целых чисел. Найти произведение четных элементов списка, добавить в конец списка данное произведение. */
+    /* Создать циклический двунаправленный список их целых чисел. Найти произведение четных элементов списка, добавить в конец списка данное произведение. */
 
     list <int> Numbers = { 5, 8, 16, 9, 21, 2, 1, -4 };
 
-    list <int> ::iterator Place, Place_1;
+    list <int> ::iterator Place;
 
-    int size, product = 1;
+    list <int> ::reverse_iterator Place_1;
 
-    /* Когда итератор Place дойдет до конца списка, то он переместиться в начало этого списка.
-       Цикл будет продолжаться пока доп. итератор Place_1, находящийся в начале списка
-       и Place не будут равны - итератор перешел к первому элементу через последний. */
-    do
+    int size = Numbers.size(), product = 1;
+
+    cout << "Изначальный список: ";
+
+    //выводим список;
+    for (Place = Numbers.begin(); Place != Numbers.end(); Place++)
     {
-        Place_1 = Numbers.begin();
+        cout << *Place << " ";
+    }
 
-        cout << "Изначальный список: ";
+    advance(Place, -size);
+    
+    cout << endl << endl;
 
-        //находим произведение четных чисел;
-        for (Place = Numbers.begin(); Place != Numbers.end(); Place++)
+    //находим произведение четных чисел;
+    for (Place_1 = Numbers.rbegin(); Place_1 != Numbers.rend(); Place_1++)
+    {
+        if (*Place_1 % 2 == 0)
         {
-            cout << *Place << " ";
-
-            if (*Place % 2 == 0)
-            {
-                product *= *Place;
-            }
+            product *= *Place_1;
         }
 
-        cout << endl << endl << "Произведение четных элементов: " << product << endl << endl;
+        if (*Place_1 == *Place)
+        {
+            advance(Place_1, -size + 1);
+            break;
+        }
+    }
+    
+    cout << "Произведение четных элементов: " << product << endl << endl;
 
-        //смавим произведение в конец списка;
-        Numbers.insert(Place, product);
-
-        size = Numbers.size();
-
-        //перемещаем итератор в начало списка;
-        std::advance(Place, -size);
-       
-    } while (Place != Place_1);
+    //добавляем элемент в конец списка;
+    for (Place = Numbers.begin(); Place != Numbers.end(); Place++)
+    {
+        if (*Place == *Place_1)
+        {
+            advance(Place, 1);
+            Numbers.insert(Place, product);
+            advance(Place, -size - 1);
+            break;
+        }
+    }
 
     cout << "Список после добавления элемента: ";
 
@@ -205,6 +217,3 @@ int main()
 
     } while (count == 0);
 }
-
-
-
